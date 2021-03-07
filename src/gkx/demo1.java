08e4a4d55b2a515d28c6fb62a73926724036522d
1789174295 <1789174295@qq.com>
2021-03-07 21:36:51 +0800

    提交src文件

---
+++ src/gkx/demo1.java
@@ -0,0 +1,9 @@
+package gkx;
+
+public class demo1 {
+    public static void main(String[] args) {
+        System.out.println("one项目发送的消息");
+        System.out.println("one项目发送消息2");
+        System.out.println("one项目发送消息2");
+    }
+}
